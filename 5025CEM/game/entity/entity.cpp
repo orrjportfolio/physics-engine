b4e4ba3a3701f03fc2fd9654e485ee05778a3675
Jackson Orr (orrj) <orrj@coventry.ac.uk>
2024-03-07 10:03:55 +0000

    converted to visual studio solution

---
+++ 5025CEM/game/entity/entity.cpp
@@ -0,0 +1,715 @@
+#include "entity.hpp"
+
+#include <cassert>
+#include <iostream>
+#include <set>
+
+#include <glm/gtx/orthonormalize.hpp>
+
+#include "../gfx/scene3d.hpp"
+#include "../geom.hpp"
+#include "octree.hpp"
+
+bool useEuler = false;
+
+void Entity::updateAll(float dt) {
+	for (uint32_t i = 0; i < num; i++) {
+		if (Entity::byIdx(i).exists() && updateFuncs[i]) {
+			lifetimes[i] += dt;
+			
+			updateFuncs[i](Entity::byIdx(i), dt);
+		}
+	}
+}
+
+void Entity::simulateAll(float dt) {
+	constexpr float fallAsleepSpeed = 0.125f;
+	
+	for (uint32_t i = 0; i < num; i++) {
+		uint32_t k = flags[i].colliderKind;
+		if ((k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) && !flags[i].isSleeping) {
+			glm::vec3 prevMin, prevMax;
+			byIdx(i).colliderBounds(&prevMin, &prevMax);
+			
+			if (useEuler) {
+				poses[i] += vels[i] * dt;
+				if (k == COLLIDER_KIND_DYNAMIC) {
+					auto newAccel = (forces[i] * invMasses[i]) + glm::vec3(0.0f, -9.8f, 0.0f);
+					vels[i] += ((accels[i] + newAccel) / 2.0f) * dt;
+					accels[i] = newAccel;
+				}
+			}
+			else {
+				poses[i] += vels[i] * dt;
+				if (k == COLLIDER_KIND_DYNAMIC) {
+					poses[i] += (accels[i] / 2.0f) * (dt * dt);
+					
+					auto newAccel = (forces[i] * invMasses[i]) + glm::vec3(0.0f, -9.8f, 0.0f);
+					vels[i] += ((accels[i] + newAccel) / 2.0f) * dt;
+					
+					accels[i] = newAccel;
+				}
+			}
+			
+			glm::vec3 min, max;
+			byIdx(i).colliderBounds(&min, &max);
+			
+			Octree::root.moveEntry(i, prevMin, prevMax, min, max);
+			
+			forces[i] = glm::vec3(0.0f);
+		}
+	}
+	
+	for (uint32_t i = 0; i < num; i++) {
+		uint32_t k = flags[i].colliderKind;
+		if (
+			((k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) && !flags[i].isSleeping) &&
+			!flags[i].colliderIsAxisAligned
+		) {
+			/*if (useEuler) {
+				auto rotAccel = (k == COLLIDER_KIND_DYNAMIC)? rotAccels[i] : glm::vec3(0.0f);
+				if (rotVels[i] != glm::vec3(0.0f) || rotAccel != glm::vec3(0.0f)) {
+					auto dRot = rotVels[i] * dt;
+					
+					auto rotVelMat = glm::mat3(
+						0, dRot.z, -dRot.y,
+						-dRot.z, 0, dRot.x,
+						dRot.y, -dRot.x, 0
+					);
+					rots[i] = glm::orthonormalize(rots[i] + (rotVelMat * rots[i]));
+				}
+				
+				if (k == COLLIDER_KIND_DYNAMIC) {
+					invInertiaTensors[i] =
+						rots[i] *
+						(glm::mat3)glm::scale(invLocalInertiaTensors[i]) *
+						glm::transpose(rots[i]);
+					
+					auto newRotAccel = torques[i];
+					rotMoms[i] += newRotAccel * dt;
+					
+					rotVels[i] = invInertiaTensors[i] * rotMoms[i];
+					
+					rotAccels[i] = newRotAccel;
+				}
+			}
+			else {*/
+				auto rotAccel = (k == COLLIDER_KIND_DYNAMIC)? rotAccels[i] : glm::vec3(0.0f);
+				if (rotVels[i] != glm::vec3(0.0f) || rotAccel != glm::vec3(0.0f)) {
+					auto dRot = (rotVels[i] * dt) + (rotAccel * 0.5f * dt * dt);
+					
+					auto rotVelMat = glm::mat3(
+						0, dRot.z, -dRot.y,
+						-dRot.z, 0, dRot.x,
+						dRot.y, -dRot.x, 0
+					);
+					rots[i] = glm::orthonormalize(rots[i] + (rotVelMat * rots[i]));
+				}
+				
+				if (k == COLLIDER_KIND_DYNAMIC) {
+					invInertiaTensors[i] =
+						rots[i] *
+						(glm::mat3)glm::scale(invLocalInertiaTensors[i]) *
+						glm::transpose(rots[i]);
+					
+					auto newRotAccel = torques[i];
+					rotMoms[i] += ((rotAccel + newRotAccel) / 2.0f) * dt;
+					
+					rotVels[i] = invInertiaTensors[i] * rotMoms[i];
+					
+					rotAccels[i] = newRotAccel;
+				}
+			//}
+			
+			torques[i] = glm::vec3(0.0f);
+		}
+	}
+	
+	for (uint32_t a = 0; a < num; a++) {
+		uint32_t aK = flags[a].colliderKind;
+		if (aK == COLLIDER_KIND_NONE) { continue; }
+		
+		auto aE = Entity::byIdx(a);
+		
+		glm::vec3 aMin, aMax;
+		aE.colliderBounds(&aMin, &aMax);
+		
+		std::vector<uint32_t> overlaps;
+		Octree::root.overlaps(aMin, aMax, overlaps);
+		
+		for (uint32_t b : overlaps) {
+			if (a == b) { continue; }
+		//for (uint32_t b = a + 1; b < num; b++) {
+			if (flags[a].isSleeping && flags[b].isSleeping) {
+				continue;
+			}
+			
+			uint32_t bK = flags[b].colliderKind;
+			if (bK == COLLIDER_KIND_NONE || (
+				(aK == COLLIDER_KIND_TRIGGER || aK == COLLIDER_KIND_KINEMATIC) &&
+				(aK == bK)
+			)) { continue; }
+			
+			if (
+				(aK == COLLIDER_KIND_TRIGGER && flags[b].isSleeping) ||
+				(bK == COLLIDER_KIND_TRIGGER && flags[a].isSleeping)
+			) { continue; }
+			
+			auto bE = Entity::byIdx(b);
+			
+			glm::vec3 bMin, bMax;
+			bE.colliderBounds(&bMin, &bMax);
+			
+			if (
+				aMin.x > bMax.x || aMin.y > bMax.y || aMin.z > bMax.z ||
+				bMin.x > aMax.x || bMin.y > aMax.y || bMin.z > aMax.z
+			) { continue; }
+			
+			auto aSK = flags[a].colliderShapeKind;
+			auto aAA = flags[a].colliderIsAxisAligned;
+			auto &aPos = poses[a];
+			auto aHS = colliderHalfSizes[a];
+			
+			auto bSK = flags[b].colliderShapeKind;
+			auto bAA = flags[b].colliderIsAxisAligned;
+			auto &bPos = poses[b];
+			auto bHS = colliderHalfSizes[b];
+			
+			glm::vec3 aVerts[8], bVerts[8];
+			
+			Overlap overlap;
+			if (aSK == ColliderShape::KIND_SPHERE) {
+				if (bSK == ColliderShape::KIND_SPHERE) {
+					overlap = Sphere::sphereOverlap(
+						aPos, aHS.x,
+						bPos, bHS.x
+					);
+				}
+				else if (bAA) {
+					overlap = Sphere::aabbOverlap(
+						aPos, aHS.x,
+						bPos - bHS, bPos + bHS
+					);
+				}
+				else {
+					overlap = Sphere::obbOverlap(
+						aPos, aHS.x,
+						bPos - bHS, bPos + bHS, bE.bodyMat(), bE.bodyMatInv()
+					);
+				}
+			}
+			else if (aAA) {
+				if (bSK == ColliderShape::KIND_SPHERE) {
+					overlap = Aabb::sphereOverlap(
+						aPos - aHS, aPos + aHS,
+						bPos, bHS.x
+					);
+				}
+				else if (bAA) {
+					overlap = Aabb::aabbOverlap(
+						aPos - aHS, aPos + aHS,
+						bPos - bHS, bPos + bHS
+					);
+				}
+				else {
+					Aabb::verts(aPos - aHS, aPos + aHS, aVerts);
+					Obb::verts(bPos, bHS, rots[b], bVerts);
+					
+					overlap = Obb::obbOverlap(
+						aPos, aVerts,
+						bPos, bVerts
+					);
+				}
+			}
+			else {
+				if (bSK == ColliderShape::KIND_SPHERE) {
+					overlap = Obb::sphereOverlap(
+						aPos - aHS, aPos + aHS, aE.bodyMat(), aE.bodyMatInv(),
+						bPos, bHS.x
+					);
+				}
+				else {
+					Obb::verts(aPos, aHS, rots[a], aVerts);
+					if (bAA) {
+						Aabb::verts(bPos - bHS, bPos + bHS, bVerts);
+					}
+					else {
+						Obb::verts(bPos, bHS, rots[b], bVerts);
+					}
+					
+					overlap = Obb::obbOverlap(
+						aPos, aVerts,
+						bPos, bVerts
+					);
+				}
+			}
+			
+			if (!overlap.exists) { continue; }
+			
+			if (aK == COLLIDER_KIND_DYNAMIC) { flags[a].isSleeping = false; }
+			if (bK == COLLIDER_KIND_DYNAMIC) { flags[b].isSleeping = false; }
+			
+			auto correct = overlap.norm * overlap.depth;
+			
+			auto &aVel = vels[a];
+			auto &bVel = vels[b];
+			
+			if (aK == COLLIDER_KIND_TRIGGER || bK == COLLIDER_KIND_TRIGGER) {
+				if (triggerFuncs[a]) {
+					triggerFuncs[a](aE, bE);
+				}
+				if (triggerFuncs[b]) {
+					triggerFuncs[b](bE, aE);
+				}
+				continue;
+			}
+			else if (aK == COLLIDER_KIND_KINEMATIC) {
+				bPos -= correct;
+				
+				glm::vec3 bNewMin, bNewMax;
+				byIdx(b).colliderBounds(&bNewMin, &bNewMax);
+				
+				Octree::root.moveEntry(b, bMin, bMax, bNewMin, bNewMax);
+			}
+			else if (bK == COLLIDER_KIND_KINEMATIC) {
+				aPos += correct;
+				
+				glm::vec3 aNewMin, aNewMax;
+				byIdx(a).colliderBounds(&aNewMin, &aNewMax);
+				
+				Octree::root.moveEntry(a, aMin, aMax, aNewMin, aNewMax);
+			}
+			else {
+				bPos -= correct / 2.0f;
+				aPos += correct / 2.0f;
+				
+				glm::vec3
+					bNewMin, bNewMax,
+					aNewMin, aNewMax;
+				byIdx(b).colliderBounds(&bNewMin, &bNewMax);
+				byIdx(a).colliderBounds(&aNewMin, &aNewMax);
+				
+				Octree::root.moveEntry(a, aMin, aMax, aNewMin, aNewMax);
+				Octree::root.moveEntry(b, bMin, bMax, bNewMin, bNewMax);
+			}
+			
+			if (collideFuncs[a]) {
+				collideFuncs[a](aE, aVel, bE, bVel, overlap.norm);
+			}
+			if (collideFuncs[b]) {
+				collideFuncs[b](bE, bVel, aE, aVel, -overlap.norm);
+			}
+			
+			size_t numContacts;
+			glm::vec3 contacts[8];
+			if (aSK == ColliderShape::KIND_SPHERE) {
+				numContacts = 1;
+				if (bSK == ColliderShape::KIND_SPHERE) {
+					contacts[0] = Sphere::sphereContact(
+						aPos, aHS.x,
+						bPos, bHS.x
+					);
+				}
+				else if (bAA) {
+					contacts[0] = Sphere::aabbContact(
+						aPos, aHS.x,
+						bPos - bHS, bPos + bHS
+					);
+				}
+				else {
+					contacts[0] = Sphere::obbContact(
+						aPos, aHS.x,
+						bPos - bHS, bPos + bHS, bE.bodyMat(), bE.bodyMatInv()
+					);
+				}
+			}
+			else if (aAA) {
+				if (bSK == ColliderShape::KIND_SPHERE) {
+					numContacts = 1;
+					contacts[0] = Aabb::sphereContact(
+						aPos - aHS, aPos + aHS,
+						bPos, bHS.x
+					);
+				}
+				else if (bAA) {
+					numContacts = 4;
+					Aabb::aabbContacts(
+						aPos - aHS, aPos + aHS,
+						bPos - bHS, bPos + bHS,
+						contacts
+					);
+				}
+				else {
+					numContacts = Obb::obbContacts(
+						aPos - aHS, aPos + aHS, glm::identity<glm::mat4>(), aVerts,
+						bPos - bHS, bPos + bHS, bE.bodyMat(), bVerts,
+						contacts
+					);
+				}
+			}
+			else {
+				if (bSK == ColliderShape::KIND_SPHERE) {
+					numContacts = 1;
+					contacts[0] = Obb::sphereContact(
+						aPos - aHS, aPos + aHS, aE.bodyMat(), aE.bodyMatInv(),
+						bPos, bHS.x
+					);
+				}
+				else if (bAA) {
+					numContacts = Obb::obbContacts(
+						aPos - aHS, aPos + aHS, aE.bodyMat(), aVerts,
+						bPos - bHS, bPos + bHS, glm::identity<glm::mat4>(), bVerts,
+						contacts
+					);
+				}
+				else {
+					numContacts = Obb::obbContacts(
+						aPos - aHS, aPos + aHS, aE.bodyMat(), aVerts,
+						bPos - bHS, bPos + bHS, bE.bodyMat(), bVerts,
+						contacts
+					);
+				}
+			}
+			
+			/*for (size_t i = 0; i < numContacts; i++) {
+				Scene3d::addDebugPoint(contacts[i], glm::vec3(1.0f), true, 0.5f);
+			}*/
+			
+			auto n = overlap.norm;
+			auto e = glm::max(bouncinesses[a], bouncinesses[b]);
+			
+			auto aInvMass = invMasses[a];
+			auto &aRotMom = rotMoms[a];
+			auto &aRotVel = rotVels[a];
+			auto aInvInertiaTensor = invInertiaTensors[a];
+			
+			auto bInvMass = invMasses[b];
+			auto &bRotMom = rotMoms[b];
+			auto &bRotVel = rotVels[b];
+			auto bInvInertiaTensor = invInertiaTensors[b];
+			
+			float impulseMags[8];
+			
+			for (size_t i = 0; i < numContacts; i++) {
+				auto contact = contacts[i];
+				
+				auto ap = contact - aPos;
+				auto bp = contact - bPos;
+				auto apVel = aVel + glm::cross(aRotVel, ap);
+				auto bpVel = bVel + glm::cross(bRotVel, bp);
+				auto abVel = apVel - bpVel;
+				auto baVel = bpVel - apVel;
+				
+				float j =
+					glm::dot(-(1.0f + e) * abVel, n) / (
+						glm::dot(n, n * (aInvMass + bInvMass)) +
+						glm::dot(n,
+							glm::cross(aInvInertiaTensor * glm::cross(ap, n), ap) +
+							glm::cross(bInvInertiaTensor * glm::cross(bp, n), bp)
+						)
+					) / (float)numContacts;
+				
+				impulseMags[i] = j;
+			}
+			
+			for (size_t i = 0; i < numContacts; i++) {
+				auto contact = contacts[i];
+				auto impulse = impulseMags[i] * n;
+				
+				//Scene3d::addDebugLine(contact, contact + impulse, glm::vec3(1.0f, 0.0f, 0.0f), true, 0.5f);
+				//Scene3d::addDebugLine(contact, contact - impulse, glm::vec3(0.0f, 1.0f, 1.0f), true, 0.5f);
+				
+				if (aK == COLLIDER_KIND_DYNAMIC) {
+					aVel += impulse * aInvMass;
+					if (!aAA) {
+						aRotMom += glm::cross(contact - aPos, impulse);
+					}
+				}
+				
+				if (bK == COLLIDER_KIND_DYNAMIC) {
+					bVel += -impulse * bInvMass;
+					if (!bAA) {
+						bRotMom += glm::cross(contact - bPos, -impulse);
+					}
+				}
+			}
+			
+			aRotVel = aInvInertiaTensor * aRotMom;
+			bRotVel = bInvInertiaTensor * bRotMom;
+			
+			float sFrict = (sFricts[a] + sFricts[b]) / 2.0f;
+			float dFrict = (dFricts[a] + dFricts[b]) / 2.0f;
+			
+			glm::vec3 frictImpulses[8];
+			
+			for (size_t i = 0; i < numContacts; i++) {
+				auto contact = contacts[i];
+				
+				auto ap = contact - aPos;
+				auto bp = contact - bPos;
+				auto apVel = aVel + glm::cross(aRotVel, ap);
+				auto bpVel = bVel + glm::cross(bRotVel, bp);
+				auto abVel = apVel - bpVel;
+				auto baVel = bpVel - apVel;
+				
+				auto tan = abVel - (n * glm::dot(abVel, n));
+				if (tan == glm::vec3(0.0f)) {
+					frictImpulses[i] = glm::vec3(0.0f);
+					continue;
+				}
+				tan = glm::normalize(tan);
+				
+				float jT =
+					glm::dot(-abVel, tan) / (
+						glm::dot(tan, tan * (aInvMass + bInvMass)) +
+						glm::dot(tan,
+							glm::cross(aInvInertiaTensor * glm::cross(ap, tan), ap) +
+							glm::cross(bInvInertiaTensor * glm::cross(bp, tan), bp)
+						)
+					) / (float)numContacts;
+				
+				float j = impulseMags[i];
+				
+				frictImpulses[i] =
+					(glm::abs(jT) <= j * sFrict)?
+						(jT * tan) :
+						(-j * tan * dFrict);
+			}
+			
+			for (size_t i = 0; i < numContacts; i++) {
+				auto contact = contacts[i];
+				auto frictionImpulse = frictImpulses[i];
+				
+				//Scene3d::addDebugLine(contact, contact + frictionImpulse, glm::vec3(1.0f, 1.0f, 0.0f), true, 0.5f);
+				//Scene3d::addDebugLine(contact, contact - frictionImpulse, glm::vec3(0.0f, 0.0f, 1.0f), true, 0.5f);
+				
+				if (aK == COLLIDER_KIND_DYNAMIC) {
+					aVel += frictionImpulse * aInvMass;
+					if (!aAA) {
+						aRotMom += glm::cross(contact - aPos, frictionImpulse);
+					}
+				}
+				
+				if (bK == COLLIDER_KIND_DYNAMIC) {
+					bVel += -frictionImpulse * bInvMass;
+					if (!bAA) {
+						bRotMom += glm::cross(contact - bPos, -frictionImpulse);
+					}
+				}
+			}
+			
+			aRotVel = aInvInertiaTensor * aRotMom;
+			bRotVel = bInvInertiaTensor * bRotMom;
+		}
+		
+		//Scene3d::addDebugPoint(poses[a], flags[a].isSleeping? glm::vec3(0.0f, 0.0f, 1.0f) : glm::vec3(1.0f, 0.0f, 0.0f), true, 0.0f);
+		
+		if ((aK == COLLIDER_KIND_KINEMATIC || aK == COLLIDER_KIND_DYNAMIC) && !flags[a].isSleeping) {
+			if (glm::length(poses[a] - prevPoses[a]) < fallAsleepSpeed * dt) {
+				restingTimes[a] += dt;
+				if (restingTimes[a] > 0.5f) {
+					flags[a].isSleeping = true;
+					restingTimes[a] = 0.0f;
+				}
+			}
+			else {
+				restingTimes[a] = 0.0f;
+			}
+		}
+		
+		prevPoses[a] = poses[a];
+	}
+}
+
+void Entity::wakeUpAllInRegion(glm::vec3 min, glm::vec3 max) {
+	std::vector<uint32_t> entities;
+	Octree::root.overlaps(min, max, entities);
+	for (auto e : entities) {
+		auto k = flags[e].colliderKind;
+		if (k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) {
+			flags[e].isSleeping = false;
+		}
+	}
+}
+
+void Entity::addAllToScene3d() {
+	for (uint32_t i = 0; i < num; i++) {
+		if (flags[i].hasMesh) {
+			Scene3d::addObject(
+				meshes[i],
+				meshMaterials[i],
+				(
+					glm::translate(poses[i]) * glm::mat4(rots[i]) *
+					meshMats[i]
+				)
+			);
+		}
+	}
+}
+
+Entity Entity::create(glm::vec3 pos, glm::mat3 const &rot) {
+	uint32_t idx;
+	if (num < CAP) {
+		idx = num++;
+	}
+	else {
+		assert(numEmpties > 0);
+		idx = empties[--numEmpties];
+	}
+	
+	uint32_t gen = gens[idx];
+	
+	lifetimes[idx] = 0.0f;
+	
+	poses[idx] = pos;
+	rots[idx] = rot;
+	
+	updateFuncs[idx] = nullptr;
+	
+	return Entity{
+		.idx = idx,
+		.gen = gen
+	};
+}
+
+void Entity::destroyAll() {
+	Octree::root.destroy();
+	Octree::root = Octree::create(nullptr, 0, glm::vec3(-200.0f), glm::vec3(500.0f));
+	
+	for (uint32_t i = 0; i < num; i++) {
+		auto e = Entity::byIdx(i);
+		if (e.exists()) { e.destroy(); }
+	}
+}
+
+void Entity::destroy() {
+	auto k = flags[idx].colliderKind;
+	if (k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) {
+		glm::vec3 min, max;
+		colliderBounds(&min, &max);
+		
+		wakeUpAllInRegion(min - glm::vec3(0.2f), max + glm::vec3(0.2f));
+	}
+	
+	flags[idx].asInt = 0;
+	gens[idx]++;
+	
+	empties[numEmpties++] = idx;
+}
+
+void Entity::setPos(glm::vec3 pos) {
+	if (flags[idx].colliderKind != COLLIDER_KIND_NONE) {
+		glm::vec3 prevMin, prevMax;
+		colliderBounds(&prevMin, &prevMax);
+		
+		poses[idx] = pos;
+		
+		glm::vec3 min, max;
+		colliderBounds(&min, &max);
+		Octree::root.moveEntry(idx, prevMin, prevMax, min, max);
+		
+		auto k = flags[idx].colliderKind;
+		if (k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) {
+			wakeUpAllInRegion(min - glm::vec3(0.2f), max + glm::vec3(0.2f));
+			
+			flags[idx].isSleeping = false;
+		}
+	}
+	else {
+		poses[idx] = pos;
+	}
+}
+
+void Entity::makeTrigger(ColliderShape shape) {
+	flags[idx].colliderKind = COLLIDER_KIND_TRIGGER;
+	flags[idx].colliderShapeKind = shape.kind;
+	flags[idx].colliderIsAxisAligned = shape.isAxisAligned;
+	
+	colliderHalfSizes[idx] = shape.halfSize;
+	
+	glm::vec3 min, max;
+	colliderBounds(&min, &max);
+	Octree::root.addEntry(idx, min, max);
+	wakeUpAllInRegion(min - glm::vec3(0.2f), max + glm::vec3(0.2f));
+}
+
+void Entity::makeKinematic(ColliderShape shape, PhysicsMaterial material) {
+	flags[idx].colliderKind = COLLIDER_KIND_KINEMATIC;
+	flags[idx].colliderShapeKind = shape.kind;
+	flags[idx].colliderIsAxisAligned = shape.isAxisAligned;
+	
+	colliderHalfSizes[idx] = shape.halfSize;
+	
+	sFricts[idx] = material.sFrict;
+	dFricts[idx] = material.dFrict;
+	bouncinesses[idx] = material.bounciness;
+	
+	vels[idx] = glm::vec3(0.0f);
+	invMasses[idx] = 0.0f;
+	forces[idx] = glm::vec3(0.0f);
+	
+	rotVels[idx] = glm::vec3(0.0f);
+	invLocalInertiaTensors[idx] = glm::vec3(0.0f);
+	invInertiaTensors[idx] = glm::mat3(0.0f);
+	torques[idx] = glm::vec3(0.0f);
+	
+	collideFuncs[idx] = nullptr;
+	triggerFuncs[idx] = nullptr;
+	
+	glm::vec3 min, max;
+	colliderBounds(&min, &max);
+	Octree::root.addEntry(idx, min, max);
+	wakeUpAllInRegion(min - glm::vec3(0.2f), max + glm::vec3(0.2f));
+}
+
+void Entity::makeDynamic(ColliderShape shape, PhysicsMaterial material, float density) {
+	glm::vec3 size = shape.halfSize * 2.0f;
+	
+	float volume =
+		(shape.kind == ColliderShape::KIND_SPHERE)?
+			(4.0f * 3.1415926535898f * shape.halfSize.x * shape.halfSize.x * shape.halfSize.x) / 3.0f :
+			size.x * size.y * size.z;
+	float mass = density * volume;
+	
+	auto invLocalInertiaTensor =
+		(shape.kind == ColliderShape::KIND_SPHERE)?
+			glm::vec3(5.0f / (2.0f * mass * shape.halfSize.x * shape.halfSize.x)) :
+			glm::vec3(
+				12.0f / (mass * ((size.y * size.y) + (size.z * size.z))),
+				12.0f / (mass * ((size.x * size.x) + (size.y * size.y))),
+				12.0f / (mass * ((size.x * size.x) + (size.z * size.z)))
+			);
+	
+	flags[idx].colliderKind = COLLIDER_KIND_DYNAMIC;
+	flags[idx].colliderShapeKind = shape.kind;
+	flags[idx].colliderIsAxisAligned = shape.isAxisAligned;
+	
+	colliderHalfSizes[idx] = shape.halfSize;
+	
+	sFricts[idx] = material.sFrict;
+	dFricts[idx] = material.dFrict;
+	bouncinesses[idx] = material.bounciness;
+	
+	vels[idx] = glm::vec3(0.0f);
+	accels[idx] = glm::vec3(0.0f);
+	invMasses[idx] = 1.0f / mass;
+	forces[idx] = glm::vec3(0.0f);
+	
+	rotMoms[idx] = glm::vec3(0.0f);
+	rotVels[idx] = glm::vec3(0.0f);
+	invLocalInertiaTensors[idx] = invLocalInertiaTensor;
+	invInertiaTensors[idx] = rots[idx] * glm::mat3(
+		invLocalInertiaTensor.x, 0, 0,
+		0, invLocalInertiaTensor.y, 0,
+		0, 0, invLocalInertiaTensor.z
+	) * glm::transpose(rots[idx]);
+	torques[idx] = glm::vec3(0.0f);
+	
+	collideFuncs[idx] = nullptr;
+	triggerFuncs[idx] = nullptr;
+	
+	glm::vec3 min, max;
+	colliderBounds(&min, &max);
+	Octree::root.addEntry(idx, min, max);
+	wakeUpAllInRegion(min - glm::vec3(0.2f), max + glm::vec3(0.2f));
+}
---
+++ 5025CEM/game/entity/entity.hpp
@@ -0,0 +1,294 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include "../gfx/gfx.hpp"
+#include "../gfx/scene3d.hpp"
+
+#undef min
+#undef max
+
+extern bool useEuler;
+
+enum ColliderKind {
+	COLLIDER_KIND_NONE,
+	COLLIDER_KIND_TRIGGER,
+	COLLIDER_KIND_KINEMATIC,
+	COLLIDER_KIND_DYNAMIC
+};
+
+struct ColliderShape {
+	enum Kind : uint8_t {
+		KIND_SPHERE,
+		KIND_BOX
+	};
+	
+	Kind kind;
+	glm::vec3 halfSize;
+	bool isAxisAligned;
+	
+	static ColliderShape sphere(float radius) {
+		return ColliderShape{
+			.kind = KIND_SPHERE,
+			.halfSize = glm::vec3(radius),
+			.isAxisAligned = false
+		};
+	}
+	
+	static ColliderShape axisAlignedBox(glm::vec3 halfSize) {
+		return ColliderShape{
+			.kind = KIND_BOX,
+			.halfSize = halfSize,
+			.isAxisAligned = true
+		};
+	}
+	
+	static ColliderShape box(glm::vec3 halfSize) {
+		return ColliderShape{
+			.kind = KIND_BOX,
+			.halfSize = halfSize,
+			.isAxisAligned = false
+		};
+	}
+};
+
+struct PhysicsMaterial {
+	float
+		sFrict, dFrict,
+		bounciness;
+};
+
+struct Entity {
+	union Flags {
+		struct {
+			uint8_t hasMesh : 1;
+			uint8_t colliderKind : 2;
+			uint8_t colliderShapeKind : 1;
+			uint8_t colliderIsAxisAligned : 1;
+			uint8_t isSleeping : 1;
+		};
+		uint8_t asInt;
+	};
+	
+	using UpdateFunc = void(*)(Entity e, float dt);
+	using CollideFunc = void(*)(Entity e, glm::vec3 eVel, Entity other, glm::vec3 otherVel, glm::vec3 norm);
+	using TriggerFunc = void(*)(Entity e, Entity other);
+	
+	static constexpr uint32_t
+		CAP = 1024;
+	static inline uint32_t
+		num;
+		
+	static inline Flags
+		flags[CAP];
+	static inline uint32_t
+		gens[CAP];
+	
+	static inline uint32_t
+		numEmpties;
+	static inline uint32_t
+		empties[CAP];
+	
+	static inline float
+		lifetimes[CAP];
+	
+	static inline glm::vec3
+		colliderHalfSizes[CAP];
+	
+	static inline float
+		sFricts[CAP],
+		dFricts[CAP],
+		bouncinesses[CAP];
+	
+	static inline glm::vec3
+		poses[CAP];
+	static inline glm::vec3
+		prevPoses[CAP];
+	static inline glm::vec3
+		vels[CAP];
+	static inline glm::vec3
+		accels[CAP];
+	static inline float
+		invMasses[CAP];
+	static inline glm::vec3
+		forces[CAP];
+	
+	static inline glm::mat3
+		rots[CAP];
+	static inline glm::vec3
+		rotMoms[CAP];
+	static inline glm::vec3
+		rotVels[CAP];
+	static inline glm::vec3
+		rotAccels[CAP];
+	static inline glm::vec3
+		invLocalInertiaTensors[CAP];
+	static inline glm::mat3
+		invInertiaTensors[CAP];
+	static inline glm::vec3
+		torques[CAP];
+	
+	static inline float
+		restingTimes[CAP];
+	
+	static inline Mesh3d
+		*meshes[CAP];
+	static inline Material
+		*meshMaterials[CAP];
+	static inline glm::mat4
+		meshMats[CAP];
+	
+	static inline UpdateFunc
+		updateFuncs[CAP];
+	static inline CollideFunc
+		collideFuncs[CAP];
+	static inline TriggerFunc
+		triggerFuncs[CAP];
+	
+	uint32_t
+		idx, gen;
+	
+	static void updateAll(float dt);
+	
+	static void simulateAll(float dt);
+	
+	static void wakeUpAllInRegion(glm::vec3 min, glm::vec3 max);
+	
+	static void addAllToScene3d();
+	
+	static Entity create(glm::vec3 pos, glm::mat3 const &rot = glm::identity<glm::mat3>());
+	
+	static Entity byIdx(uint32_t idx) {
+		return Entity{idx, gens[idx]};
+	}
+	
+	static void destroyAll();
+	
+	void destroy();
+	
+	bool exists() {
+		return
+			flags[idx].asInt != 0 &&
+			gens[idx] == gen;
+	}
+	
+	glm::vec3 pos() { return poses[idx]; }
+	
+	glm::vec3 vel() { return vels[idx]; }
+	
+	glm::mat3 rot() { return rots[idx]; }
+	
+	glm::vec3 right() { return rots[idx][0]; }
+	
+	glm::vec3 up() { return rots[idx][1]; }
+	
+	glm::vec3 forward() { return rots[idx][2]; }
+	
+	glm::vec3 rotVel() { return rotVels[idx]; }
+	
+	glm::mat4 bodyMat() {
+		return
+			glm::translate(poses[idx]) *
+			glm::mat4(glm::transpose(rots[idx])) *
+			glm::translate(-poses[idx]);
+	}
+	
+	glm::mat4 bodyMatInv() {
+		return
+			glm::translate(poses[idx]) *
+			glm::mat4(rots[idx]) *
+			glm::translate(-poses[idx]);
+	}
+	
+	ColliderShape colliderShape() {
+		return ColliderShape{
+			.kind = (ColliderShape::Kind)flags[idx].colliderKind,
+			.halfSize = colliderHalfSizes[idx],
+			.isAxisAligned = (bool)flags[idx].colliderIsAxisAligned
+		};
+	}
+	
+	void colliderBounds(glm::vec3 *oMin, glm::vec3 *oMax) {
+		glm::vec3 r = colliderHalfSizes[idx];
+		if (
+			flags[idx].colliderShapeKind == ColliderShape::KIND_BOX &&
+			!flags[idx].colliderIsAxisAligned
+		) {
+			r = glm::vec3(std::max({r.x, r.y, r.z}) * 1.732051f);
+		}
+		
+		*oMin = poses[idx] - r;
+		*oMax = poses[idx] + r;
+	}
+	
+	void setPos(glm::vec3 pos);
+	
+	void setVel(glm::vec3 vel) {
+		vels[idx] = vel;
+		auto k = flags[idx].colliderKind;
+		if (k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) {
+			flags[idx].isSleeping = false;
+		}
+	}
+	
+	void setRot(glm::mat3 rot) {
+		rots[idx] = rot;
+		auto k = flags[idx].colliderKind;
+		if (k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) {
+			flags[idx].isSleeping = false;
+		}
+	}
+	
+	void setRotVel(glm::vec3 rotVel) {
+		rotVels[idx] = rotVel;
+		auto k = flags[idx].colliderKind;
+		if (k == COLLIDER_KIND_KINEMATIC || k == COLLIDER_KIND_DYNAMIC) {
+			flags[idx].isSleeping = false;
+		}
+	}
+	
+	void addForce(glm::vec3 force) {
+		flags[idx].isSleeping = false;
+		forces[idx] += force;
+	}
+	
+	void addForceAt(glm::vec3 force, glm::vec3 pos) {
+		flags[idx].isSleeping = false;
+		forces[idx] += force;
+		torques[idx] += glm::cross(pos - poses[idx], force);
+	}
+	
+	void makeTrigger(ColliderShape shape);
+	
+	void makeKinematic(ColliderShape shape, PhysicsMaterial material);
+	
+	void makeDynamic(ColliderShape shape, PhysicsMaterial material, float density);
+	
+	void addMesh(
+		Mesh3d &mesh,
+		Material &material,
+		glm::mat4 mat = glm::identity<glm::mat4>()
+	) {
+		flags[idx].hasMesh = true;
+		
+		meshes[idx] = &mesh;
+		meshMaterials[idx] = &material;
+		meshMats[idx] = mat;
+	}
+	
+	void setUpdateFunc(UpdateFunc func) {
+		updateFuncs[idx] = func;
+	}
+	
+	void setCollideFunc(CollideFunc func) {
+		collideFuncs[idx] = func;
+	}
+	
+	void setTriggerFunc(TriggerFunc func) {
+		triggerFuncs[idx] = func;
+	}
+};
